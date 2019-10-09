2da8d9ca4363e1b3cbcb0835a4bc8bf42ec3c966
junxiang wang <function@wzw.com>
2019-10-09 16:18:26 +0800

    feat 新增hr组件

---
+++ components/diy/HrComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <view :style="{paddingTop:style.margin+'px',paddingBottom:style.margin+'px'}">
+    <view
+      :style="{borderBottomWidth:style.height+'px',borderBottomColor:style.color,borderBottomStyle:hr.config.type}"
+      class="hr"></view>
+  </view>
+</template>
+<script>
+  import {deepCopyStrict} from "../../common/tool";
+  const styleDefault = {
+    color: '#e7e7e7',
+    margin: '4',
+    height: '1',
+  };
+  export default {
+    props: {
+      index: {
+        required: true,
+      },
+      confData: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+    data() {
+      return {
+        hr: {},
+      };
+    },
+    computed: {
+      style() {
+        return deepCopyStrict(styleDefault, this.hr.style);
+      },
+    },
+    watch: {
+
+    },
+    components: {},
+    methods: {
+
+    },
+    created() {
+      //用这个来搞事啊
+      this.hr = this.confData
+    }
+  }
+</script>
+
+
+<style scoped lang="stylus">
+  .hr
+    height 0
+    width 100%
+</style>
