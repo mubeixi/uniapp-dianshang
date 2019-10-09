8b1bd685da0aa7a44bd37b687f15b6340bfdea46
junxiang wang <function@wzw.com>
2019-10-09 16:19:10 +0800

    feat 新增Text组件

---
+++ components/diy/TextComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <view class="text">
+    <view :style="{fontSize:text.style.fontSize+'px',color:text.style.color,backgroundColor:text.style.bgColor,textAlign:text.style.textAlign}"
+         class="text">{{text.value.content}}
+    </view>
+  </view>
+</template>
+<script>
+  import {deepCopyStrict} from "../../common/tool";
+
+  const styleDefault = {
+    color: '#333',
+    bgColor: '#e6e6e6',
+    fontSize: '14',
+    textAlign: 'left',
+  }
+  export default {
+    props: {
+      index: {
+        required: true,
+      },
+      confData: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+    data() {
+      return {
+        text: {},
+      };
+    },
+    computed: {
+      style() {
+        return deepCopyStrict(styleDefault, this.text.style);
+      },
+
+    },
+    watch: {
+
+    },
+    components: {},
+    methods: {
+
+    },
+    created() {
+      //用这个来搞事啊
+      this.text = this.confData
+    }
+  }
+</script>
+
+
+<style scoped lang="stylus">
+  .text
+    width 100%
+</style>
