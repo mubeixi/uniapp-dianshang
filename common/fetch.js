92ee4dcc7d5814c084380db9f28264bf49363a2c
jiahaihong <527538629@qq.com>
2019-10-18 18:23:06 +0800

    env

--- common/fetch.js
+++ common/fetch.js
@@ -47,6 +47,7 @@ const fetch = function (act, param,options = false,url='/api/little_program/shop
   param.User_ID = get_User_ID();
   param.Users_ID = get_Users_ID();   //Users_ID  写死
   param.appid = get_Appid();
+	param.env = GET_ENV();
   // param.User_ID = 3;
   // param.Users_ID = 'wkbq6nc2kc';
 
