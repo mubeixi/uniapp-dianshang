cd90887155fd0bdce3f0899e875a5f0382464ef0
jiahaihong <527538629@qq.com>
2019-10-29 10:57:02 +0800

    支付页  用pay组件

--- pages/pay/pay.vue
+++ pages/pay/pay.vue
@@ -109,22 +109,6 @@
 			</div>
 			<div class="submit" @click="submit">去支付</div>
 		</div>
-<!--		<popup-layer ref="popupLayer" :direction="'top'">-->
-<!--			<div class="iMbx">-->
-<!--				<div class="c_method" v-for="(item,index) in pay_arr" @click="chooseType(index)" :key="index">-->
-<!--					{{item}} <text>￥{{orderInfo.Order_Fyepay}}</text>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</popup-layer>-->
-
-<!--		Order_ID: this.Order_ID,-->
-<!--		pay_type: this.pay_type,-->
-<!--		pay_money: this.orderInfo.Order_Fyepay, // 剩余支付的钱-->
-<!--		use_money: this.user_money , // 使用的余额-->
-<!--		user_pay_password: this.user_pay_password, //余额支付密码-->
-<!--		need_invoice: this.need_invoice,-->
-<!--		invoice_info: this.invoice_info,-->
-<!--		order_remark: this.order_remark-->
 		<pay-components
 				ref="payLayer"
 				:Order_ID="Order_ID"
@@ -134,6 +118,7 @@
 				:invoice_info="invoice_info"
 				:order_remark="order_remark"
 				:paySuccessCall="paySuccessCall"
+				:payFailCall = "payFailCall"
 		/>
 
 	</div>
@@ -187,6 +172,7 @@
 				need_invoice: 0, // 是否需要发票
 				showDirect: false, // 是否直接显示付款方式
 				pay_arr: [], // 支付方式
+				user_money: 0,
 			}
 		},
 		onLoad(options) {
@@ -336,8 +322,6 @@
 						return;
 					}
 				}
-
-
 
 				// #endif
 
@@ -665,6 +649,7 @@
 					// this.orderInfo.Order_TotalPrice = money;
 					return;
 				}
+				this.pay_money = Number(this.orderInfo.Order_TotalPrice - money).toFixed(2);
 				this.orderInfo.Order_Fyepay = Number(this.orderInfo.Order_TotalPrice - money).toFixed(2);
 			},
 			// 余额支付开关
@@ -674,9 +659,11 @@
 					this.openMoney = true;
 					this.user_money = Number(this.orderInfo.Order_Yebc).toFixed(2);
 					this.orderInfo.Order_Fyepay = Number(this.orderInfo.Order_TotalPrice - this.user_money).toFixed(2);
+					this.pay_money = Number(this.orderInfo.Order_TotalPrice - this.user_money).toFixed(2);
 				} else {
 					this.openMoney = false;
 					this.orderInfo.Order_Fyepay = Number(this.orderInfo.Order_TotalPrice).toFixed(2);
+					this.pay_money = Number(this.orderInfo.Order_TotalPrice).toFixed(2);
 					this.user_money = 0;
 				}
 			},
@@ -712,6 +699,8 @@
 			},
 			// 去支付
 			submit() {
+				this.$refs.payLayer.show();
+				return;
 				// 发票信息
 				if (this.need_invoice && this.invoice_info == '') {
 					uni.showToast({
