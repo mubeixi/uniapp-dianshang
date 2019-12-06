c86b8d1278a5749345e4284ff4a8f1e85382e1d7
jiahaihong <527538629@qq.com>
2019-12-06 11:36:56 +0800

    修改 进货部分

--- pages/title/title.vue
+++ pages/title/title.vue
@@ -8,7 +8,7 @@
               </view>
               <view class="methods" v-if="isShowMethod">
                   <view class="method" @click="method(1)">进货记录</view>
-                  <view class="method" @click="method(2)">门店信息</view>
+                  <view class="method" v-if="is_pingtai == 0" @click="method(2)">门店信息</view>
               </view>
 
           </view>
@@ -41,12 +41,17 @@ export default {
 		},
 		bgcolor:{
 			default: '#f8f8f8'
-        },
-        // 右上角是三个点
-        dot: {
-            type: Boolean,
-            default: false
-        },
+    },
+		// 右上角是三个点
+		dot: {
+				type: Boolean,
+				default: false
+		},
+		// 是否是平台进货，平台进货的时候，不应该有门店信息
+		is_pingtai: {
+			type: Number,
+			default: 0
+		}
 	},
     data: function() {
 	    return {
@@ -132,15 +137,16 @@ export default {
                 right: 0;
                 background: #666;
                 color: #fff;
-                height: 130rpx;
+								display: flex;
+								flex-direction: column;
                 width: 167rpx;
                 font-size: 24rpx;
                 z-index: 1000;
                 .method {
                     line-height: 65rpx;
                 }
-                .method:nth-child(1) {
-                    border-bottom: 2rpx dotted #fff;
+                .method:nth-child(2) {
+                    border-top: 2rpx dotted #fff;
                 }
             }
         }
