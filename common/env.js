dd70e683cb24ff439c6ebbd96c5bf393348f8890
junxiang wang <function@wzw.com>
2020-02-19 10:43:37 +0800

    迁移到公网

--- common/env.js
+++ common/env.js
@@ -10,13 +10,13 @@ export const apiBaseUrl = isDev ? '' : '';//h5调试为了跨域https://new401.b
 // #endif
 
 // #ifndef H5
-export const apiBaseUrl = 'https://new401t.bafangka.com';
+export const apiBaseUrl = 'https://new401.bafangka.com';
 // #endif
 
 //后面的正式环境地址,每次打包要从新切换.
 //许总又要求,可以打包一套多个站点部署
 const devStatic = 'https://new401t.bafangka.com'
-const prodStatic = 'https://new401t.bafangka.com'
+const prodStatic = 'https://new401.bafangka.com'
 
 export const staticUrl = isDev ?devStatic:prodStatic;
 export const apiKey = '458f_$#@$*!fdjisdJDFHUk4%%653154%^@#(FSD#$@0-T';
