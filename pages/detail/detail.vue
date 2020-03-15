24f783faaab175229b77ba13a1e1d19db9db88c4
junxiang wang <function@wzw.com>
2020-03-15 16:59:56 +0800

    直播功能为微信小程序专享

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -219,14 +219,16 @@
 		</form>
 	</popupLayer>
 
+	  <!-- #ifdef MP-WEIXIN -->
 	  <view class="liveBox" style="bottom: 70px" v-if="liveList.length===1">
-		  <navigator  :url="'plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id='+liveList[0].roomid">
-			  <image class="icon-live" src="/static/live/logo.png"></image>
-		  </navigator>
+	  		  <navigator  :url="'plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id='+liveList[0].roomid">
+	  			  <image class="icon-live" src="/static/live/logo.png"></image>
+	  		  </navigator>
 	  </view>
 	  <view class="liveBox" style="bottom: 70px" v-if="liveList.length>1">
-		  <image @click="toLive" class="icon-live" src="/static/live/logo.png"></image>
+	  		  <image @click="toLive" class="icon-live" src="/static/live/logo.png"></image>
 	  </view>
+	  <!-- #endif -->
   </div>
 </template>
 <script>
@@ -452,10 +454,13 @@ export default {
 		// #endif
 
 
+		// #ifdef MP-WEIXIN
 		getLiveInfo().then(res=>{
+			// console.log('getLiveInfo data is ',res)
 			this.liveList = res.data.room_info
 			this.liveCount = res.data.count
 		})
+		// #endif
 
 
 	},
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -101,6 +101,8 @@
 			<div class="item" @click="tabbarFn(3)">购物车</div>
 			<div class="item" @click="tabbarFn(4)">个人中心</div>
 		</div> -->
+		
+		<!-- #ifdef MP-WEIXIN -->
 		<view class="liveBox" v-if="liveList.length===1">
 			<navigator  :url="'plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id='+liveList[0].roomid">
 				<image class="icon-live" src="/static/live/logo.png"></image>
@@ -109,7 +111,8 @@
 		<view class="liveBox" v-if="liveList.length>1">
 		<image @click="toLive" class="icon-live" src="/static/live/logo.png"></image>
 		</view>
-
+		<!-- #endif -->
+		
 	</view>
 
 </template>
@@ -279,11 +282,14 @@
                 })
             }
 
+			// #ifdef MP-WEIXIN
 			getLiveInfo().then(res=>{
-				console.log('getLiveInfo data is ',res)
+				// console.log('getLiveInfo data is ',res)
 				this.liveList = res.data.room_info
 				this.liveCount = res.data.count
 			})
+			// #endif
+			
 
 		},
 		mounted(){
