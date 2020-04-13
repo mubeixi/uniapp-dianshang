ee11f96008159e69b1ad5d162a7099bcfed085ef
junxiangwang <271938312@qq.com>
2020-04-13 17:08:34 +0800

    fix 【ID1002821】前端Local Storage内的user_id没有定期清理,后台user_id有过期时间,接口返回登录信息已过期,导致/pages/login/login循环跳转

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1009,7 +1009,10 @@ export default {
 						url: '/pages/order/check?cart_key=DirectBuy'
 					})
 				}
-			}).catch(()=>{})
+			}).catch(()=>{
+				this.isSubmit = false;
+			})
+			this.isSubmit = false;
 			//确定加入购物车
 			this.hideNativeEleShow = false
 			this.$refs.cartPopu.close();
