cb97d6155a3a16d5dd89ebeffe6f6423d320ae59
junxiangwang <271938312@qq.com>
2020-04-22 18:32:38 +0800

    分支

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -209,14 +209,14 @@
 		</form>
 	</popupLayer>
 	  <!-- #ifdef MP-WEIXIN -->
-	 <!-- <view class="liveBox" :style="{bottom: '70px'}" v-if="liveList.length===1">
+	 <view class="liveBox" :style="{bottom: '70px'}" v-if="liveList.length===1">
 	  		  <navigator  :url="'plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id='+liveList[0].roomid">
 	  			  <image class="icon-live" src="/static/live/logo.png"></image>
 	  		  </navigator>
 	  </view>
 	  <view class="liveBox" :style="{bottom: '70px'}" v-if="liveList.length>1">
 	  		  <image @click="toLive" class="icon-live" src="/static/live/logo.png"></image>
-	  </view> -->
+	  </view>
 	  <!-- #endif -->
   </div>
 </template>
