66bb87790d5afd88a6a61ec33808d67c0431d237
junxiangwang <271938312@qq.com>
2020-05-06 09:46:52 +0800

    fix 修复isCheckLogin会清空ls中内容引起的配置不生效问题

--- common/interceptors.js
+++ common/interceptors.js
@@ -78,11 +78,7 @@ export const ajax = (url, method, data, options) => {
                             error(res.msg)
 
                             //重置用户信息
-                            let users_id = ls.get('users_id');
-                            let owner_id = ls.get('owner_id');
-                            ls.clear();
-                            ls.set('owner_id',owner_id)
-                            ls.set('users_id', users_id);
+                            store.commit('SET_USER_INFO',{})
                             // #ifdef H5
                             sessionStorage.removeItem('is_send_usrlog')
                             // #endif
