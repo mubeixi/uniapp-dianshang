1f94ab0f6c0f0270f08dab6e96484ef8cd42d2ac
junxiangwang <271938312@qq.com>
2020-05-18 18:49:03 +0800

    进一步优化定制项目配置

--- common/env.js
+++ common/env.js
@@ -3,37 +3,25 @@ export const isCustom = false;
 
 export const isDev = process.env.NODE_ENV === 'production' ? false : true;
 
-// #ifdef APP-PLUS || MP-TOUTIAO
 export const APP_USERS_ID = 'wkbq6nc2kc';//'	mrw5iqvhs2 xjgjd88jee  wkbq6nc2kc  05eiz8cqtw o1rcshfk39';
-// #endif
 
-let _apiBaseUrl =''
 
-// #ifndef H5
-_apiBaseUrl = 'https://new401t.bafangka.com';
-// #endif
+export const apiBaseUrl = 'https://new401t.bafangka.com';
 
-export const apiBaseUrl = _apiBaseUrl;
-
-//后面的正式环境地址,每次打包要从新切换.
-//许总又要求,可以打包一套多个站点部署
-
-const devStatic = 'https://new401t.bafangka.com'
-
-let prodStatic = ''
-
+let staticUrl = ''
 //app 和小程序还是需要手动设置staticUrl
 // #ifndef H5
-prodStatic = 'https://new401.bafangka.com'
+const appProdStatic = 'https://new401.bafangka.com'
+staticUrl = appProdStatic
 // #endif
 
 //h5就自动获取
 // #ifdef H5
 const { origin } = location
-prodStatic =  origin
+staticUrl = origin
 // #endif
+export const staticUrl = staticUrl;
 
-export const staticUrl = prodStatic;
 
 export default {
     apiBaseUrl
