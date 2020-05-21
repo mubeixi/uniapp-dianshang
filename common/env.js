6e35e031848cfb4b7715b25e36692e1c0d4b3057
junxiangwang <271938312@qq.com>
2020-05-21 11:25:34 +0800

    fix 修复env配置的缺陷，进一步精简

--- common/env.js
+++ common/env.js
@@ -1,28 +1,26 @@
-//是否为定制项目
-export const isCustom = false;
+// 是否为定制项目
+export const isCustom = false
 
-export const isDev = process.env.NODE_ENV === 'production' ? false : true;
+export const isDev = process.env.NODE_ENV !== 'production'
 
-export const APP_USERS_ID = 'wkbq6nc2kc';//'	mrw5iqvhs2 xjgjd88jee  wkbq6nc2kc  05eiz8cqtw o1rcshfk39';
+export const APP_USERS_ID = 'wkbq6nc2kc' // mrw5iqvhs2 xjgjd88jee  wkbq6nc2kc  05eiz8cqtw o1rcshfk39';
 
-
-export const apiBaseUrl = 'https://new401t.bafangka.com';
+export const apiBaseUrl = 'https://new401t.bafangka.com'
 
 let _staticUrl = ''
-//app 和小程序还是需要手动设置staticUrl
+// app 和小程序还是需要手动设置staticUrl
 // #ifndef H5
 const appProdStatic = 'https://new401.bafangka.com'
 _staticUrl = appProdStatic
 // #endif
 
-//h5就自动获取
+// h5就自动获取
 // #ifdef H5
 const { origin } = location
-_staticUrl = origin
+_staticUrl = isDev ? 'https://new401t.bafangka.com' : origin
 // #endif
 
 export const staticUrl = _staticUrl
-
 
 export default {
     apiBaseUrl
