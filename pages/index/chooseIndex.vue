231c7f47bf4cbec5a1ebc09d45ee14166e82a756
mbx0829@163.com <mubeixi0829>
2020-07-03 17:43:23 +0800

    修复直播在首页显示

--- pages/index/chooseIndex.vue
+++ pages/index/chooseIndex.vue
@@ -95,14 +95,14 @@
     <!-- #endif -->
 
     <!-- #ifdef MP-WEIXIN -->
-    <view class="liveBox" v-if="liveList.length===1">
+    <!-- <view class="liveBox" v-if="liveList.length===1">
       <navigator :url="'plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id='+liveList[0].roomid">
         <image class="icon-live" src="/static/live/logo.png"></image>
       </navigator>
     </view>
     <view class="liveBox" v-if="liveList.length>1">
       <image @click="toLive" class="icon-live" src="/static/live/logo.png"></image>
-    </view>
+    </view> -->
     <!-- #endif -->
   </view>
 </template>
